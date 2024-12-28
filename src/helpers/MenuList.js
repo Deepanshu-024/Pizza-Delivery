f3287ac9522451d0f4e487bbb06a2d1f5ab535e5
Deepanshu-024 <goyal.deepanshu024@gmail.com>
2024-12-28 20:39:52 +0530

    first commit

---
+++ src/helpers/MenuList.js
@@ -0,0 +1,39 @@
+import Pepperoni from "../assets/pepperoni.jpg";
+import Margherita from "../assets/margherita.jpg";
+import PedroTechSpecial from "../assets/pedrotechspecial.jpg";
+import Vegan from "../assets/vegan.jpg";
+import Pineapple from "../assets/pineapple.jpg";
+import Expensive from "../assets/expensive.jpg";
+
+export const MenuList = [
+  {
+    name: "Pepperoni Pizza",
+    image: Pepperoni,
+    price: 200,
+  },
+  {
+    name: "Margherita Pizza",
+    image: Margherita,
+    price: 150,
+  },
+  {
+    name: "Yumm Special Pizza",
+    image: PedroTechSpecial,
+    price: 250,
+  },
+  {
+    name: "Vegan Pizza",
+    image: Vegan,
+    price: 80,
+  },
+  {
+    name: "Pineapple Pizza",
+    image: Pineapple,
+    price: 90,
+  },
+  {
+    name: "Very Expensive Pizza",
+    image: Expensive,
+    price: 300,
+  },
+];
